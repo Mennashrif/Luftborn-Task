58b387378bb3639efd9aa63687dce8424a02abf3
mennashrif2000 <mennatullah.sherif@asset.com.eg>
2024-11-10 23:16:23 +0200

    First Commit

---
+++ ReportSystem/ReportSystem.API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReportSystem.Application.Features.Account.Queries.Login;
+using ReportSystem.Application.Features.Common.DTO;
+using ReportSystem.Application.Features.Users.Queries.LoggedInUser;
+
+namespace ReportSystem.API.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class AccountController(IMediator mediator) : ApiBaseController
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<ActionResult<ResponseDTO>> Login([FromBody] LoginQuery command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet("Profile")]
+        public async Task<ActionResult<ResponseDTO>> GetProfile()
+        {
+            var response = await _mediator.Send(new LoggedInUserQuery());
+            return Ok(response);
+        }
+
+    }
+}
---
+++ ReportSystem/ReportSystem.API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReportSystem.Application.Features.Common.DTO;
+using ReportSystem.Application.Features.Users.Commands.AddUser;
+using ReportSystem.Application.Features.Users.Commands.DeleteUser;
+using ReportSystem.Application.Features.Users.Commands.EditUser;
+using ReportSystem.Application.Features.Users.Queries.GetUsers;
+
+namespace ReportSystem.API.Controllers
+{
+
+    [Authorize]
+    public class UserController(IMediator mediator) : ApiBaseController
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseDTO>> Get()
+        {
+            var response = await _mediator.Send(new GetUsersQuery());
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseDTO>> Post([FromBody] AddUserCommand addUserCommand)
+        {
+            var response = await _mediator.Send(addUserCommand);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ResponseDTO>> Put([FromBody] EditUserCommand editUserCommand)
+        {
+            var response = await _mediator.Send(editUserCommand);
+            return Ok(response);
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult<ResponseDTO>> Delete(int userId)
+        {
+            var response = await _mediator.Send(new DeleteUserCommand() { UserId = userId });
+            return Ok(response);
+        }
+    }
+
+}
